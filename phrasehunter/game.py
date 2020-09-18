f8e6a00474f22b9266479064d012c2ad0a31fc4e
Spencer <spencerccroy@gmail.com>
2020-09-18 06:51:48 -0700

    Add Game and Phrase Class

--- phrasehunter/game.py
+++ phrasehunter/game.py
@@ -1,1 +1,7 @@
 # Create your Game class logic in here.
+class Game:
+    def __init__(self):
+        self.missed = 0
+        self.phrases = []
+        self.activephrase = None
+        self.guesses = [" "]
